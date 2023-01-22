a5905f72c76966facf064079e3e6a0963c0b26f4
jaskalra31 <78143792+jaskalra31@users.noreply.github.com>
2023-01-22 13:41:45 +0530

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const client = axios.create({ baseURL: "http://localhost:8000/api" });
+const client = axios.create({ baseURL: "https://63ccd7e8c2d9a54d421deb7e--lovely-cuchufli-681ab8.netlify.app/api" });
 
 export default client;
